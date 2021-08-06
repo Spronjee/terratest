18842685beb3871db6b09cb28c7406f8e004f917
CHUKHLIEBOV IHOR <50753797+Spronjee@users.noreply.github.com>
2021-08-06 13:39:49 +0300

    Add files via upload

--- examples/main.tf
+++ examples/main.tf
@@ -1,8 +1,3 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# PIN TERRAFORM VERSION TO >= 0.12
-# The examples have been upgraded to 0.12 syntax
-# ---------------------------------------------------------------------------------------------------------------------
-
 terraform {
   # This module is now only being tested with Terraform 0.13.x. However, to make upgrading easier, we are setting
   # 0.12.26 as the minimum version, as that version added support for required_providers with source URLs, making it
@@ -10,26 +5,7 @@ terraform {
   required_version = ">= 0.12.26"
 }
 
-# ---------------------------------------------------------------------------------------------------------------------
-# BASIC TERRAFORM EXAMPLE
-# See test/terraform_aws_example.go for how to write automated tests for this code.
-# ---------------------------------------------------------------------------------------------------------------------
-
-data "template_file" "example" {
-  template = var.example
+# website::tag::1:: The simplest possible Terraform module: it just outputs "Hello, World!"
+output "hello_world" {
+  value = "Hello, World!"
 }
-
-data "template_file" "example2" {
-  template = var.example2
-}
-
-resource "local_file" "example" {
-  content  = "${data.template_file.example.rendered} + ${data.template_file.example2.rendered}"
-  filename = "example.txt"
-}
-
-resource "local_file" "example2" {
-  content  = data.template_file.example2.rendered
-  filename = "example2.txt"
-}
-
